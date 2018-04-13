882079e9be3d61951a7fafe00154bcbccc3c3101
nakaba9696 <38350108+nakaba9696@users.noreply.github.com>
2018-04-13 20:24:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,5 +14,14 @@ async def on_message(message):
     react_num = random.randint(0, len(emojis) - 1)
     await client.add_reaction(message, emojis[react_num])
 
+# test
+@client.event
+async def on_message(message):
+     if message.content.startswith("こんにちは"):
+          if client.user != message.author:
+              await client.send_message(message.channel, "こんにちは,#{message.author.name}さん")
+
+    
+    
 if __name__ == "__main__":
     client.run("")
